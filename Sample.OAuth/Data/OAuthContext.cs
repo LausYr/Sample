1cd2193cba46f1b05b0882697d671e732400fbff
LausYr <sultanaevalexey@gmail.com>
2021-10-08 18:34:17 +0500

    Создание класса OAuthContext

---
+++ Sample.OAuth/Data/OAuthContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Sample.Entities.Models;
+
+namespace Sample.OAuth.Data
+{
+    public class OAuthContext : IdentityDbContext<ApplicationUser>
+    {
+        public OAuthContext(DbContextOptions<OAuthContext> options)
+             : base(options)
+        {
+        }
+        public DbSet<ApplicationUser> Accounts { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().HasAlternateKey(u => u.PhoneNumber);
+            builder.Entity<ApplicationUser>().HasAlternateKey(u => u.Email);
+            base.OnModelCreating(builder);
+        }
+    }
+}
